e41ba4c4adf651500afcb6dfbd5333b26fc23373
Tural Mehtiyev <tmehtiyev2019@ada.edu.az>
2023-07-28 21:55:53 -0400

    removed some useless files from the product catalog app folder

--- app/product_catalog_microservice/app/models.py
+++
@@ -1,9 +0,0 @@
-import database
-from sqlalchemy import Column, Integer, String, Boolean
-
-class Product(Base):
-    __tablename__="products_test"
-    id=Column(Integer, primary_key=True, nullable=False)
-    name=Column(String,nullable=False )
-    price=Column(Integer,nullable=False)
-    quantity=Column(Integer,nullable=False)

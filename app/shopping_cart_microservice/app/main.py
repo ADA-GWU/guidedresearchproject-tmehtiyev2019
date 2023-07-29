5e402ea75c011e1c5d29392f691ed6bf83ecdd7c
Tural Mehtiyev <tmehtiyev2019@ada.edu.az>
2023-07-29 16:36:31 -0400

    Updated the shopping cart service

--- app/shopping_cart_microservice/app/main.py
+++ app/shopping_cart_microservice/app/main.py
@@ -3,9 +3,7 @@ from pydantic import BaseModel
 from fastapi.responses import Response
 import psycopg2
 from psycopg2.extras import RealDictCursor
-import time
 import requests
-
 
 
 try:
@@ -23,28 +21,28 @@ except psycopg2.Error as e:
 try:
     cur = conn.cursor()
 
-    # Create a 'cart' table
-    create_cart_table_query = '''
-    CREATE TABLE IF NOT EXISTS cart(
+    # Create a 'carts' table
+    create_carts_table_query = '''
+    CREATE TABLE IF NOT EXISTS carts(
         id SERIAL PRIMARY KEY,
         customer_id INT NOT NULL,
         status VARCHAR NOT NULL
     )
     '''
 
-    # Create a 'cart_item' table
-    create_cart_item_table_query = '''
-    CREATE TABLE IF NOT EXISTS cart_item(
+    # Create a 'cart_items' table
+    create_cart_items_table_query = '''
+    CREATE TABLE IF NOT EXISTS cart_items(
         id SERIAL PRIMARY KEY,
         product_id INT NOT NULL,
         quantity INT NOT NULL,
         cart_id INT,
-        FOREIGN KEY (cart_id) REFERENCES cart (id)
+        FOREIGN KEY (cart_id) REFERENCES carts (id)
     )
     '''
 
-    cur.execute(create_cart_table_query)
-    cur.execute(create_cart_item_table_query)
+    cur.execute(create_carts_table_query)
+    cur.execute(create_cart_items_table_query)
 
     conn.commit()
     print("Tables created successfully")
@@ -54,10 +52,8 @@ except psycopg2.Error as e:
 
 
 class CartItem(BaseModel):
-    id: int
     product_id: int
     quantity: int
-    cart_id: int
 
 
 class Cart(BaseModel):
@@ -82,18 +78,23 @@ def get_cart(customer_id: int):
 
 @app.post("/carts/{customer_id}", status_code=status.HTTP_201_CREATED)
 def add_item_to_cart(customer_id: int, cart_item: CartItem):
-    # First, check the quantity of the product in the Product Catalog service
     response = requests.get(f"http://localhost:8000/products/{cart_item.product_id}")
     if response.status_code == 200:
         product = response.json()["product_detail"]
         if product['quantity'] >= cart_item.quantity:
-            # If the product has enough quantity, add it to the cart
-            cur.execute("INSERT INTO carts (customer_id, product_id, quantity, status) VALUES (%s, %s, %s, 'Active')",
-                        (customer_id, cart_item.product_id, cart_item.quantity))
+            cur.execute("SELECT id FROM carts WHERE customer_id = %s AND status = 'Active'", (customer_id,))
+            cart = cur.fetchone()
+            if cart is None:
+                cur.execute("INSERT INTO carts (customer_id, status) VALUES (%s, 'Active') RETURNING id", 
+                            (customer_id,))
+                cart_id = cur.fetchone()["id"]
+            else:
+                cart_id = cart["id"]
+            cur.execute("INSERT INTO cart_items (cart_id, product_id, quantity) VALUES (%s, %s, %s)",
+                        (cart_id, cart_item.product_id, cart_item.quantity))
             conn.commit()
             return {"message": "Item added to cart successfully"}
         else:
-            # If the product does not have enough quantity, return an error
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, 
                                 detail="Not enough product in stock")
     else:
@@ -103,7 +104,7 @@ def add_item_to_cart(customer_id: int, cart_item: CartItem):
 
 @app.delete("/carts/{customer_id}/{product_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_item_from_cart(customer_id: int, product_id: int):
-    cur.execute("DELETE FROM carts WHERE customer_id = %s AND product_id = %s AND status = 'Active'",
+    cur.execute("DELETE FROM cart_items WHERE cart_id IN (SELECT id FROM carts WHERE customer_id = %s) AND product_id = %s",
                 (customer_id, product_id))
     conn.commit()
     if cur.rowcount == 0:
@@ -114,13 +115,11 @@ def delete_item_from_cart(customer_id: int, product_id: int):
 
 @app.put("/carts/{customer_id}/{product_id}")
 def update_item_in_cart(customer_id: int, product_id: int, updated_cart_item: CartItem):
-    # First, check the quantity of the product in the Product Catalog service
     response = requests.get(f"http://localhost:8000/products/{product_id}")
     if response.status_code == 200:
         product = response.json()["product_detail"]
         if product['quantity'] >= updated_cart_item.quantity:
-            # If the product has enough quantity, update the item in the cart
-            cur.execute("UPDATE carts SET quantity = %s WHERE customer_id = %s AND product_id = %s AND status = 'Active'",
+            cur.execute("UPDATE cart_items SET quantity = %s WHERE cart_id IN (SELECT id FROM carts WHERE customer_id = %s AND status = 'Active') AND product_id = %s",
                         (updated_cart_item.quantity, customer_id, product_id))
             conn.commit()
             if cur.rowcount == 0:
@@ -128,13 +127,11 @@ def update_item_in_cart(customer_id: int, product_id: int, updated_cart_item: Ca
                                     detail=f"Product with id: {product_id} does not exist in the cart of customer with id: {customer_id}")
             return {"message": "Item quantity updated successfully"}
         else:
-            # If the product does not have enough quantity, return an error
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, 
                                 detail="Not enough product in stock")
     else:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f"Product with id {product_id} not found")
-
 
 
 @app.put("/carts/{customer_id}", status_code=status.HTTP_202_ACCEPTED)
